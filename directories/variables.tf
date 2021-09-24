e825d8c30fd073f4373a05b66cb78267953e2fab
fabian <macbook@macbooks-MacBook-Pro.local>
2021-09-24 17:32:58 +0200

    directories structure

---
+++ directories/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "AWS Access Key ID"
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "environment" {
+  description = "AWS Secret Key"
+}
