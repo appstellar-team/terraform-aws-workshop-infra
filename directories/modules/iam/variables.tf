768bdfbb654bdaab22d7ad96b8ab911c37ccf6c0
fabian <macbook@macbooks-MacBook-Pro.local>
2021-09-24 17:34:53 +0200

    iam module

---
+++ directories/modules/iam/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "Environment where resource will get created."
+}
+
+variable "name" {
+  description = "Name of the IAM User"
+}
+
+variable "bucket" {
+  description = "Bucket name"
+}
