d0c5c80cdaa1b60f3c6a3a0af6df7430ca1fc45b
fabian <macbook@macbooks-MacBook-Pro.local>
2021-09-25 12:47:30 +0200

    added content-type

--- monolith/env/index.js
+++ monolith/env/index.js
@@ -3,13 +3,14 @@ exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     headers: {
+      "Content-Type": "application/json",
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET",
     },
-    body: JSON.stringify(
-      "Hello from IaC with Terraform Workshop - Test Lambda!"
-    ),
+    body: JSON.stringify({
+      message: "Hello from IaC with Terraform Workshop - Test Lambda!",
+    }),
   };
   return response;
 };
