35f5efef580f8e2903a4ba63440f051b588120b2
fabian <macbook@macbooks-MacBook-Pro.local>
2021-09-25 12:34:21 +0200

    Cors headers

--- monolith/env/index.js
+++ monolith/env/index.js
@@ -2,7 +2,14 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from IaC with Terraform Workshop - Test Lambda!"),
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    },
+    body: JSON.stringify(
+      "Hello from IaC with Terraform Workshop - Test Lambda!"
+    ),
   };
   return response;
 };
