9653012fe8ca5c39370dbed79732873b02d88640
Mehmetali Shaqiri <tali@appstellar.io>
2021-09-25 16:06:16 +0200

    formatting and fixed duplicate naming module

--- workspaces/env/main.tf
+++ workspaces/env/main.tf
@@ -1,22 +1,22 @@
 # Create S3 Bucket
 module "bucket" {
-  source      = "../../modules/s3"
+  source                  = "../../modules/s3"
   name                    = format("%s-%s", var.bucket_name, var.environment)
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  tags                    =  {
+  tags = {
     environment = var.environment
   }
 }
 
 
-module "bucket" {
-  source      = "../../modules/s3"
-  name                    = format("%s-%s-sample", var.bucket_name, var.environment)
-  tags                    =  {
+module "bucket_two" {
+  source = "../../modules/s3"
+  name   = format("%s-%s-sample", var.bucket_name, var.environment)
+  tags = {
     environment = var.environment
-    author = "tali"
+    author      = "tali"
   }
 }
